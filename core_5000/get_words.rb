359d2d1fafcd04230284ae12460efb8ca063e05f
Jeff Zohrab <jzohrab@gmail.com>
2020-03-24 13:40:21 -0600

    Get core words full data

--- core_5000/get_words.rb
+++ core_5000/get_words.rb
@@ -5,14 +5,27 @@ require_relative '../lib/LanguageUtils'
 
 module Core5000
 
-  def self.get_words()
+  # Returns array of hashes for each line in rawdata.txt.
+  def self.get_entries()
     lines = File.readlines(File.join(__dir__, 'rawdata.txt'))
-    words = lines.map do |lin|
+    hsh = lines.map do |lin|
       parts = lin.split('|')
       raise "Bad line #{lin}" unless parts.size == 5
-      parts[1]
+      {
+        rank: parts[0],
+        word: parts[1],
+        type: parts[2],
+        translation: parts[3],
+        sample: parts[4].strip
+      }
     end
-    words
+    hsh
+  end
+
+  ENTRIES = self.get_entries()
+
+  def self.get_words()
+    ENTRIES.map { |e| e[:word] }
   end
 
   WORDS = self.get_words()
@@ -24,14 +37,23 @@ module Core5000
     return false
   end
 
+  def self.possible_matches(candidate)
+    m = ENTRIES.select { |e| LanguageUtils::are_like(candidate, e[:word]) }
+    m
+  end
+
 end
 
 
 # Manual testing and checking
 if $0 == __FILE__ then
-  words = Core5000::get_words()
-  puts "#{words.size} words total"
-  puts "#{Core5000::WORDS.size} total, from class constant."
-  dups = words.select { |e| words.count(e) > 1 }.uniq
-  puts "#{dups.size} dups total"
+  if ARGV.size == 1 then
+    puts Core5000::possible_matches(ARGV[0]).inspect
+  else
+    words = Core5000::get_words()
+    puts "#{words.size} words total"
+    puts "#{Core5000::WORDS.size} total, from class constant."
+    dups = words.select { |e| words.count(e) > 1 }.uniq
+    puts "#{dups.size} dups total"
+  end
 end
