cad269cc68fe64b9121797716af81a4ee7338b18
Jeff Zohrab <jzohrab@gmail.com>
2022-01-19 10:56:19 -0600

    Bulk example lookup.

---
+++ freedict_examples/README.md
@@ -0,0 +1,11 @@
+Given a piped text file of words, gets example sentences from https://es.thefreedictionary.com/.
+
+Useful when trying to learn how a given word is used, to create your own sentences.
+
+Usage:
+
+`cat <somefile.txt> | ruby examples.rb`
+
+Try the sample file:
+
+`cat ./sample.txt | ruby examples.rb`
---
+++ freedict_examples/examples.rb
@@ -0,0 +1,32 @@
+#  cat somefile.txt | ruby examples.rb
+
+require_relative '../lib/TheFreeDictionary'
+require 'json'
+
+f = TheFreeDictionary.new(nil)
+f.debug = true
+
+STDIN.each_line do |lin|
+  word = lin.strip
+
+  # Shortcut if blank
+  next if word == ''
+
+  result = f.lookup(word)
+  # puts result.inspect
+
+  puts "\n\n"
+  puts result[:root]
+
+  examples = result[:definitions].map do |d|
+    d[:example]
+  end
+  examples =
+    examples.
+      select { |d| !d.nil? }.
+      sort { |a, b| a.length <=> b.length }.
+      map { |e| "#{e.capitalize}" }.
+      map { |e| e =~ /\.$/ ? e : "#{e}." }
+
+  puts examples
+end
